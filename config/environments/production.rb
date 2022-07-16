fc20ad0524bef91d804d3cc65bcb29bdbc053dfa
matildamort <matilda.jane.morton@gmail.com>
2022-07-16 12:00:11 +1000

    Deployment stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,6 +113,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.assets.compile = true
-  config.serve_static_assets = true
+  
 
 end
