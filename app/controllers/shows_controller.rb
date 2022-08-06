1077c300b1c0d8b610b854cb11b40afcf236c1d5
matildamort <matilda.jane.morton@gmail.com>
2022-08-06 10:11:17 +1000

    uncommented update

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -24,14 +24,13 @@ class ShowsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /shows/1
-  # def update - Not being used in this release
-  #   if @show.update(show_params)
-  #     render json: @show
-  #   else
-  #     render json: @show.errors, status: :unprocessable_entity
-  #   end
-  # end
+  PATCH/PUT /shows/1
+    if @show.update(show_params)
+      render json: @show
+    else
+      render json: @show.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /shows/1
   def destroy
