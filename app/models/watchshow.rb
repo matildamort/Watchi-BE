29fbeb20ead235f6e3af669cf071c9a54e37d5c9
matildamort <matilda.jane.morton@gmail.com>
2022-08-06 13:33:47 +1000

    changed watchlist and watchshows dependancies

--- app/models/watchshow.rb
+++ app/models/watchshow.rb
@@ -1,4 +1,5 @@
 class Watchshow < ApplicationRecord
 has_many :shows, dependent: :destroy
+has_many :watchlists, dependent: :destroy
 # belongs_to :watchlist, optional: :true, dependent: :destroy
 end
