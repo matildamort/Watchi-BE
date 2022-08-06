defc2751e6c62bb4612f2e23dd5210eeac2c1297
matildamort <matilda.jane.morton@gmail.com>
2022-08-06 15:07:13 +1000

    adjusted watchshow create method to create instead of new

--- app/models/watchshow.rb
+++ app/models/watchshow.rb
@@ -1,5 +1,5 @@
 class Watchshow < ApplicationRecord
-    belongs_to :watchlists #dependent: :destroy
+    belongs_to :watchlist #dependent: :destroy
     # belongs_to :show #dependent: :destroy
     has_one :show
 # belongs_to :watchlist, optional: :true, dependent: :destroy
