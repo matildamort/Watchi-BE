f0147e31a13741909f85ced710289c57b1abed5b
matildamort <matilda.jane.morton@gmail.com>
2022-08-06 14:04:25 +1000

    changed watchlist and watchshows dependancies

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_one_attached :picture
-    belongs_to :watchshows
+    has_many :watchshows
    
     def picture_url
         Rails.application.routes.url_helpers.url_for(picture) if picture.attached?
